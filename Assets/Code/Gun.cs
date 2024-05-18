e7f72548368e24371d1d2e0ebed4a0d546d2d3c6
towurzer <t.wurzer.2004@gmail.com>
2024-05-18 03:28:24 +0200

    Added Gun & fixed compiler error (hopefully)

---
+++ Assets/Code/Gun.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform shotPos;
+    public GameObject bullet;
+    public int amountOfBullets;
+    public float spread, bulletSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        // Check if the object where this script is attached is a child of the player
+        if (transform.parent != null && transform.parent.parent != null && transform.parent.parent.CompareTag("Player"))
+        {
+            for (int i = 0; i < amountOfBullets; i++)
+            {
+                GameObject bulletIns = Instantiate(bullet, shotPos.position, transform.rotation);
+                Rigidbody2D rb = bulletIns.GetComponent<Rigidbody2D>();
+                //Vector2 dir = transform.rotation * Vector2.up;
+                Vector2 dir = transform.up;
+                Vector2 pdir = Vector2.Perpendicular(dir) * Random.Range(-spread, spread);
+                rb.velocity = (dir + pdir) * bulletSpeed;
+
+            }
+        }
+    }
+}
