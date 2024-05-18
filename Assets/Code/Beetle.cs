20d3deb423e1893650373ae7f56ede760646d612
wolphi <phiwolfger@edu.aau.at>
2024-05-18 22:00:23 +0200

    Flame animation (cursed)

---
+++ Assets/Code/Beetle.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Beetle : Enemy
+{
+    public override void attack(Collision2D collision)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void die()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void takeBulletDamage(Collision2D collision)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void takeJumpDamage(Collision2D collision)
+    {
+        throw new System.NotImplementedException();
+    }
+}
