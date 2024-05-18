2ca8085aa709f22fca70e8a8414150470c34cec9
towurzer <t.wurzer.2004@gmail.com>
2024-05-18 04:21:41 +0200

    added basic HP-System for Player

--- Assets/Code/movement.cs
+++ Assets/Code/movement.cs
@@ -2,50 +2,97 @@ using UnityEngine;
 
 public class movableObject : MonoBehaviour
 {
-[SerializeField] public float speed = 5.0f;
-[SerializeField] public float jumpForce = 7.0f;
+    [SerializeField] public float speed = 5.0f;
+    [SerializeField] public float jumpForce = 7.0f;
 
-public Rigidbody2D rigidbody;
-private KeyCode RIGHT = KeyCode.D;
-private KeyCode LEFT = KeyCode.A;
-private KeyCode UP = KeyCode.W;
+    public Rigidbody2D rigidbody;
+    private KeyCode RIGHT = KeyCode.D;
+    private KeyCode LEFT = KeyCode.A;
+    private KeyCode UP = KeyCode.W;
 
-private bool doubleJump = false;
+    private bool doubleJump = false;
 
-void Start()
-{
-    rigidbody = GetComponent<Rigidbody2D>();
-    rigidbody.gravityScale = 2.0f;
-}
-void Update()
-{
-    if (Input.GetKey(RIGHT))
+
+    // HP System:
+    [SerializeField] public int health = 30;
+    [SerializeField] public int shield = 100;
+    private bool isBlocking = false;
+    private KeyCode BLOCK = KeyCode.LeftShift;
+
+
+
+    void Start()
     {
-        transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
-        transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.gravityScale = 2.0f;
     }
-
-    if (Input.GetKey(LEFT))
+    void Update()
     {
-        transform.position += new Vector3(-speed * Time.deltaTime, 0.0f, 0.0f);
-        transform.rotation = new Quaternion(0.0f, 180.0f, 0.0f, 0.0f);
-    }
 
-    if (Input.GetKeyDown(UP))
-    {
-        // if the player is on the gound jump and set doubleJump to false
-        // else if player is in air, but hasn't double jumped, jump and set doubleJump to true
-        if (Mathf.Abs(rigidbody.velocity.y) < 0.001f)
+        if (Input.GetKeyDown(BLOCK))
         {
-            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            doubleJump = false;
+            isBlocking = true;
         }
-        else if (!doubleJump && rigidbody.velocity.y < 0)
+        if (Input.GetKeyUp(BLOCK))
         {
-            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            doubleJump = true;
-        
+            isBlocking = false;
+        }
+
+
+        // --------------------------------------------------------------------------------
+        // Insert anything over this line, further down an execution can not be guaranteed
+
+        if (isBlocking)
+        {
+            // If the player is blocking, do not allow movement
+            return;
+        }
+
+        if (Input.GetKey(RIGHT))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+            transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(LEFT))
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0.0f, 0.0f);
+            transform.rotation = new Quaternion(0.0f, 180.0f, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKeyDown(UP))
+        {
+            // if the player is on the gound jump and set doubleJump to false
+            // else if player is in air, but hasn't double jumped, jump and set doubleJump to true
+            if (Mathf.Abs(rigidbody.velocity.y) < 0.001f)
+            {
+                rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                doubleJump = false;
+            }
+            else if (!doubleJump && rigidbody.velocity.y < 0)
+            {
+                rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                doubleJump = true;
+
+            }
         }
     }
-}
+
+
+    void takeDamage(int damage)
+    {
+        if (isBlocking)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                health += shield;
+                shield = 0;
+            }
+        }
+        else
+        {
+            health -= damage;
+        }
+    }
 }
