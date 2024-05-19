91c3c7f42b5d34360fa7568c5ef9e4972656e343
towurzer <t.wurzer.2004@gmail.com>
2024-05-19 16:55:10 +0200

    Fixed CompilerError regarding Sniper and Camera Problem (downside: lost aiming functionality)

--- Assets/Code/Sniper.cs
+++ Assets/Code/Sniper.cs
@@ -37,12 +37,19 @@ public class Sniper : MonoBehaviour
     { 
         double rotationAdd = plantFacing == 0 ? 0 : 180;
         // Rotate the object to face the mouse if the angle between the x-axis and the mouse is less than 30 degrees
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        if (angle < 30 + rotationAdd && angle > -30 + rotationAdd)
-        {
-            transform.rotation = Quaternion.Euler(0, 0, angle);
+        
+        //Vector3 mousePos = CinemachineCamera.ScreenToWorldPoint(Input.mousePosition);
+        if (Camera.main != null){
+
+        
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Vector3 mousePos = Camera.current.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            if (angle < 30 + rotationAdd && angle > -30 + rotationAdd)
+                {
+                transform.rotation = Quaternion.Euler(0, 0, angle);
+            }
         }
     }
     void Shoot()
@@ -50,7 +57,6 @@ public class Sniper : MonoBehaviour
     
         cooldownTimer = cooldown; 
         GameObject bulletIns = Instantiate(bullet, shotPos.position, transform.rotation);
-        Rigidbody2D rb = bulletIns.GetComponent<Rigidbody2D>();
         SoundManager.Instance.playRandom("a_salt_rifle", 0.1); 
             
     }
