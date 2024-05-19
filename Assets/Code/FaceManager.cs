ab01f73bb39fbb354399d3b9598f073e5174c51e
David Schwaiger <ds@mscg.at>
2024-05-19 16:55:49 +0200

    Win and Lose Screen

--- Assets/Code/FaceManager.cs
+++ Assets/Code/FaceManager.cs
@@ -14,8 +14,11 @@ public class FaceManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
         
     }
 
